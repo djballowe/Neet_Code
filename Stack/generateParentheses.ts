afa8db47163b05e101f65da17c8c2faeacdb5c4c
djballowe <djballowe@gmail.com>
2024-01-16 07:44:59 -0800

    generate paren

--- Stack/generateParentheses.ts
+++ Stack/generateParentheses.ts
@@ -1,57 +1,23 @@
-class TreeNode {
-    val: string;
-    r: TreeNode | null;
-    l: TreeNode | null;
-    constructor(val: string) {
-        this.val = val;
-        this.r = null;
-        this.l = null;
-    }
-}
-
-class Tree {
-    root: TreeNode | null;
-    constructor() {
-        this.root = null;
-    }
-    insert(node: TreeNode) {
-        if (!this.root) {
-            this.root = node;
-        } else {
-            let currNode = this.root;
-            let count = 1;
-            while (count !== currNode.val.length - 1) {
-                if (node.val[count] === '(') {
-                    currNode = currNode.l as TreeNode
-                } else {
-                    currNode = currNode.r as TreeNode
-                }
-                count++;
-            }
-            direction === 'l' ? currNode.l = node : currNode.r = node 
-        }
-        return node;
-    }
-}
-
 function generateParenthesis(n: number): string[] {
-    let tree = new Tree();
-    let l: string[] = [];
-    let r: string[] = [];
-    for (let i = 0; i < n; i++) {
-        l.push('(');
-        r.push(')');
-    }
-    tree.insert({ prevNode: l.pop() as string, r: null, l: null });
-    const fillTree = (l: string[], r: string[]) => {
-        if (l.length) {
-            l.pop();
-            fillTree(l, r);
-        } else if (r.length) {
-            r.pop();
-            fillTree(l, r);
+    let stack: string[] = [];
+    let ans: string[] = [];
+
+    const backTrack = (open: number, closed: number) => {
+        if (open === n && closed === n) {
+            ans.push(stack.join(''));
+            return;
         }
-        return;
-    };
-    return ['hi'];
+        if (open < n) {
+            stack.push('(');
+            backTrack(open + 1, closed);
+            stack.pop()
+        }
+        if (open > closed) {
+            stack.push(')');
+            backTrack(open, closed + 1);
+            stack.pop()
+        }
+    }
+    backTrack(0, 0);
+    return ans;
 }

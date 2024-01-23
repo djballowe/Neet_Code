183784e25a4f51819631d9746f95f4a9cd29d4e2
djballowe <djballowe@gmail.com>
2024-01-22 20:36:34 -0800

    water trap

--- TwoPointers/trapping.cpp
+++ TwoPointers/trapping.cpp
@@ -1,5 +1,5 @@
 #include <algorithm>
-#include <iostream>
+#include <cstdlib>
 #include <vector>
 
 typedef unsigned int uint;
@@ -7,38 +7,35 @@ typedef unsigned int uint;
 class Solution {
 public:
     int trap(std::vector<int> &height) {
-        std::vector<uint> stack;
-        size_t i = 0;
-        size_t l, r;
-        uint waterTotal = 0, water = 0;
-        uint maxHeight = std::numeric_limits<uint>::max();
+        size_t n = height.size();
+        int *left = static_cast<int *>(std::malloc(sizeof(int) * n)),
+            *right = static_cast<int *>(std::malloc(sizeof(int) * n)),
+            *diff = static_cast<int *>(std::malloc(sizeof(int) * n));
+        int maxLeft = 0, maxRight = 0, waterTotal = 0;
 
-        while (i < height.size()) {
-            if (height[i] > height[i + 1]) {
-                l = i;
-                r = i + 1;
-                stack.push_back(height[l]);
-                maxHeight = height[i];
-                int count = 0;
-                while (height[r] < maxHeight) {
-                    stack.push_back(height[r]);
-                    r++;
-                }
-                for (int value : stack) {
-                    std::cout << value << " ";
-                }
-                maxHeight = std::min(maxHeight, static_cast<uint>(height[r]));
-                while (stack.size()) {
-                    water = maxHeight - stack.back();
-                    waterTotal += water;
-                    stack.pop_back();
-                }
-                std::cout << "Total: " << waterTotal << std::endl;
-                i = r;
-            } else {
-                i++;
-            }
+        for (int i = 0; i < height.size(); i++) {
+            maxLeft = i == 0 ? 0 : std::max(maxLeft, height[i - 1]);
+            left[i] = maxLeft;
         }
+
+        for (int i = n - 1; i >= 0; i--) {
+            maxRight = i == n - 1 ? 0 : std::max(maxRight, height[i + 1]);
+            right[i] = maxRight;
+        }
+
+        for (int i = 0; i < n; i++) {
+            diff[i] = std::min(left[i], right[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int water = diff[i] - height[i];
+            waterTotal += water <= 0 ? 0 : water;
+        }
+
+        std::free(left);
+        std::free(right);
+        std::free(diff);
+
         return waterTotal;
     }
 };

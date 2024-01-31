fea22320a3a40fd98843a4e653f1a4a20610976b
djballowe <djballowe@gmail.com>
2024-01-30 21:06:31 -0800

    moke

--- BinarySearch/min_eating_speed.cpp
+++ BinarySearch/min_eating_speed.cpp
@@ -1,48 +1,37 @@
-#include <vector>
-#include <iostream>
 #include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <vector>
 
-using std::vector;
 using std::cout;
 using std::endl;
+using std::vector;
 
 class Solution {
 public:
     int minEatingSpeed(vector<int> &piles, int h) {
         auto max = std::max_element(piles.begin(), piles.end());
-        
-        int l = piles.size();
+
+        int l = 1;
         int r = *max;
+
+        int res = r;
+        cout << r << endl;
 
         while (l <= r) {
             int m = l + (r - l) / 2;
-            int bph = canEat(m, h, piles);
-            if (bph == 0) {
-                return m;
-            } else if (bph > 0) {
+            long int hours = 0;
+            for (int i = 0; i < piles.size(); i++) {
+                hours += std::ceil((double) piles[i] / m);
+            }
+            if (hours <= h) {
+                res = std::min(res, m);
                 r = m - 1;
             } else {
                 l = m + 1;
             }
-
         }
-        return 1;        
-    }
-
-private:
-    int canEat(int m, int h, vector<int> &piles) {
-        vector<int> tempPiles = piles;
-        int tempHours = h;
-        for (int i = 0; i < tempPiles.size(); i++) {
-            while (tempPiles[i] > 0 && tempHours > 0) {
-                tempPiles[i] -= m;
-                tempHours--;
-            }
-            if (tempHours == 0 && i != tempPiles.size() - 1) {
-                return -1;
-            }
-        }
-        cout << m << " : " << tempHours << endl;
-        return tempHours > 0 ? 1 : 0;
+        return res;
     }
 };

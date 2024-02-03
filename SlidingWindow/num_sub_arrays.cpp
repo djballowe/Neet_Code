25cd4f1920283b26593e1a9a11471af57559f2cc
djballowe <djballowe@gmail.com>
2024-02-03 10:45:55 -0800

    sliding window

---
+++ SlidingWindow/num_sub_arrays.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numOfSubarrays(vector<int> &arr, int k, int threshold) {
+        int sum = 0;
+        int average = 0;
+        int ans = 0;
+
+        for (int l = 0; l < arr.size(); r++) {
+            sum += arr[l + k - 1]
+        }
+        return ans;
+    }
+};

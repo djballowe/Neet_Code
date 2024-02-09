e204f97d1e566e29f21f31f233a8ada884c73e86
djballowe <djballowe@gmail.com>
2024-02-08 18:05:14 -0800

    min window hard

--- SlidingWindow/min_window.cpp
+++ SlidingWindow/min_window.cpp
@@ -1,33 +1,61 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
-#include <vector>
 
 using namespace std;
 
 class Solution {
 private:
-    int hasChars(vector<int> &freq) { return false; }
+    int hasChars(unordered_map<char, int> &map) {
+        for (auto &pair : map) {
+            if (pair.second > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 public:
-    int minWindow(string s, string t) {
+    string minWindow(string s, string t) {
+        if (t.size() > s.size()) {
+            return "";
+        }
         unordered_map<char, int> map;
         int l = 0;
         int r = 0;
-        int shortest = s.size();
+        string ans;
+        pair<int, int> shortest = {0, s.size()};
+        bool valid = false;
 
         for (int i = 0; i < t.size(); i++) {
             map.find(t[i]) != map.end() ? map[t[i]] += 1 : map[t[i]] = 1;
         }
 
-        for (const auto& pair : map) {
-            cout << pair.first << ":" << pair.second << "\n";
-        }
-
         while (r < s.size()) {
-
+            if (map.find(s[r]) != map.end()) {
+                map[s[r]]--;
+            }
+            if (hasChars(map)) {
+                while (hasChars(map)) {
+                    valid = true;
+                    if (shortest.second - shortest.first >= r - l + 1) {
+                        shortest.first = l;
+                        shortest.second = r;
+                        cout << r << endl;
+                    }
+                    if (map.find(s[l]) != map.end()) {
+                        map[s[l]]++;
+                    }
+                    l++;
+                }
+            }
+            r++;
         }
 
-        return 1;
+
+        for (int i = shortest.first; i <= shortest.second; i++) {
+            ans += s[i];
+        }
+        return valid ? ans : "";
     }
 };

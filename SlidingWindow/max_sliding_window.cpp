a6d35298cd0ade46f19f8aca27951a256e2fdd57
djballowe <djballowe@gmail.com>
2024-02-09 21:06:32 -0800

    max window deque

--- SlidingWindow/max_sliding_window.cpp
+++ SlidingWindow/max_sliding_window.cpp
@@ -1,36 +1,31 @@
-#include <algorithm>
-#include <climits>
-#include <iostream>
+#include <deque>
 #include <vector>
 
 using namespace std;
 
 class Solution {
-private:
-    int calculateLargest(vector<int> &nums, int l, int r) {
-        int largest = 0;
-        for (int i = l; i <= r; i++) {
-            largest = max(largest, nums[i]);
-        }
-        return largest;
-    }
-
 public:
     vector<int> maxSlidingWindow(vector<int> &nums, int k) {
         vector<int> ans;
-        int l = 1;
-        int largest = INT_MIN;
+        deque<int> deque;
+        int l = 0;
+        int r = 0;
 
-        for (int r = k - 1; r < nums.size(); r++) {
-            if (r == k - 1 || nums[l - 1] == largest) {
-                cout << r << ", " << "";
-                largest = calculateLargest(nums, l, r);
-            } else {
-                largest = max(largest, nums[r]);
+        while (r < nums.size()) {
+            while (!deque.empty() && nums[r] > deque.back()) {
+                deque.pop_back();
             }
-            ans.push_back(largest);
-            l++;
-            //cout << largest << ", " << "";
+
+            deque.push_back(nums[r]);
+
+            if (r + 1 >= k) {
+                ans.push_back(deque.front());
+                if (nums[l] == deque.front()) {
+                    deque.pop_front();
+                }
+                l++;
+            }
+            r++;
         }
         return ans;
     }

d143230861ab96c28d2d544dc8cbb2e2cceabcf3
djballowe <djballowe@gmail.com>
2024-06-13 17:47:19 -0700

    daily I missed

---
+++ ArraysAndHashing/min_moves_to_seat.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minMovesToSeat(vector<int> &seats, vector<int> &students) {
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        int moves = 0;
+
+        for (int i = 0; i < students.size(); i++) {
+            moves += abs(students[i] - seats[i]);
+        }
+
+        return moves;
+    }
+};

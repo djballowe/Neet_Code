f5116704a32899f02e4a32d486d1fa0722ca8300
djballowe <djballowe@gmail.com>
2024-07-07 20:04:41 -0700

    find winner daily and half of swap pairs

---
+++ ArraysAndHashing/find_the_winner.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+using namespace std;
+
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        int nodes = 1;
+        ListNode* head = new ListNode(nodes);
+        ListNode* currNode = head;
+        while (nodes < n) {
+            currNode->next = new ListNode(++nodes);
+            currNode = currNode->next;
+        }
+        currNode->next = head;
+
+        ListNode* person = head;
+        ListNode* prevNode = currNode;
+        int count = 0;
+        while (person->val != prevNode->val) {
+            count = k;
+            while (count > 1) {
+                person = person->next;
+                prevNode = prevNode->next;
+                count--;
+            }
+            prevNode->next = person->next;
+            person = prevNode->next;
+        }
+
+        return person->val;
+    }
+};
+
